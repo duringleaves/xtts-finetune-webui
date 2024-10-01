7766e5011bb03a8e3753dffd3220a77a4c326e9a
Durin Gleaves <durin@duringleaves.com>
2024-10-01 09:02:29 -0700

    Update xtts_demo.py

--- xtts_demo.py
+++ xtts_demo.py
@@ -270,7 +270,7 @@ if __name__ == "__main__":
                             compute_type = "float32"
                         
                         asr_model = WhisperModel(whisper_model, device=device, compute_type=compute_type)
-                        train_meta, eval_meta, audio_total_size = format_audio_list(audio_files, asr_model=asr_model, target_language=language, out_path=out_path, gradio_progress=progress, speaker_name)
+                        train_meta, eval_meta, audio_total_size = format_audio_list(audio_files, asr_model=asr_model, target_language=language, out_path=out_path, gradio_progress=progress, speaker_name=speaker_name)
                     except:
                         traceback.print_exc()
                         error = traceback.format_exc()
